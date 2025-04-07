5b622d6cc65b737384bf96affa5bd9fb596386a0
Riritastic <enriquepr2000@hotmail.com>
2025-04-07 14:42:33 -0400

    commit inicial

---
+++ src/main/java/com/example/Dispositivo.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Dispositivo {
+    private String marca;
+    private String memoryRam;
+    private String memoryStorage;
+    private String procesador;
+    private String modelo;
+    private int fabricationYear;
+    private int precio;
+    private int stock;
+}
